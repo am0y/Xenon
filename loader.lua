4e13f860b11ede5bf2363a6be493fe4644dd5a5d
naj <wizebg403@gmail.com>
2024-11-03 13:07:51 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -8,7 +8,7 @@ if getgenv().Aimbot then return end
 
 --// Load Aimbot V2 (Raw)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/am0y/xeno/refs/heads/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/am0y/Xenon/main/main.lua"))()
 
 --// Variables
 
